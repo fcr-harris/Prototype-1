4931d0db2b8fecb529ad86480b3c52485fa3ca0d
E. Harris <56174820+fcr-harris@users.noreply.github.com>
2019-11-04 13:17:55 -0800

    Step 4
    2.4 completed

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  public float speed = 20f;
-  public float turnSpeed;
-  public float horizontalInput;
+  private float speed = 10f;
+  private float turnSpeed = 35f;
+  private float horizontalInput;
+  private float forwardInput;
   
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
       horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
 
-      // We'll move the vehicle forward.
-      transform.Translate(Vector3.forward * Time.deltaTime * speed);
-      transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+      // Moves the car forward based on vertical input
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+      // Moves the car based on horizontal input
+      transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
